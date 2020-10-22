ea680216d60a375492f5620fd8ec5ae447849b73
Maleex99 <72606947+Maleex99@users.noreply.github.com>
2020-10-22 12:28:39 +0200

    Add files via upload

--- Loi_Mouvement.py
+++ Loi_Mouvement.py
@@ -5,46 +5,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def getA(a0, a1, a2, posB,tf):
+    ''' Fonction pour obtenir A'''
     return (posB - a0 - a1*tf - a2 * tf**2)/ tf**3
 
 def getB(a1, a2, vitB, tf):
+    ''' Fonction pour obtenir B'''
     return (vitB - a1 - 2*a2 * tf) / tf**2
 
 def getC(a2, accB, tf):
+    ''' Fonction pour obtenir C'''
     return (accB - 2*a2) / tf
 
 def geta0(posA):
+    ''' Fonction pour obtenir a0'''
     return posA
 
 def geta1(vitA):
+    ''' Fonction pour obtenir a1'''
     return vitA
 
 def geta2(accA):
+    ''' Fonction pour obtenir a2'''
     return accA / 2.0
 
 def geta3(A, B, C):
+    ''' Fonction pour obtenir a3'''
     return 10*A - 4*B + C/2.0
 
 def geta4(A, B, C, tf):
+    ''' Fonction pour obtenir a4'''
     return (-15*A + 7*B - C) / tf
 
 def geta5(A, B, C, tf):
+    ''' Fonction pour obtenir a5'''
     return (12*A - 6*B + C) / (2.0 * tf**2)
     
 def getTfQ(posA, vitA, accA, posB, vitB, accB, vMax, aMax):
+    ''' Fonction pour obtenir Tf optimal à 1ms prés'''
     vMaxCalc = sys.float_info.max
     aMaxCalc = sys.float_info.max
     
-    pas = 1
+    pas = 0.001
 
     tf = pas
     a0 = geta0(posA)
     a1 = geta1(vitA)
     a2 = geta2(accA)
     
-    i = 0
-    
-    while i < 10**4:
+    while tf < 10**4:   #On ne calcule pas pour un temps Tf supérrieur à 10 000s
+        # Obtention des cohéficients de nos équations
         A = getA(a0, a1, a2, posB, tf)
         B = getB(a1, a2, vitB, tf)
         C = getC(a2, accB, tf)
@@ -53,11 +62,13 @@ def getTfQ(posA, vitA, accA, posB, vitB, accB, vMax, aMax):
         a4 = geta4(A, B, C, tf)
         a5 = geta5(A, B, C, tf)
     
+        # Création des équations nésséssaire pour verifier que aMax(tf) < aMax et vMax(tf) < vMax
         vit = np.poly1d([5*a5, 4*a4, 3*a3, 2*a2, a1])
         acc = np.poly1d([20*a5, 12*a4, 6*a3, 2*a2])
         derv_acc = acc.deriv()
         derv2_acc = acc.deriv(2)
         
+        # Récupération de vMax(tf)
         rootAcc = acc.r
         
         rootAcc = rootAcc[rootAcc > 0]
@@ -70,6 +81,7 @@ def getTfQ(posA, vitA, accA, posB, vitB, accB, vMax, aMax):
         
         vMaxCalc = max(np.abs(vit(xVMax)))
         
+        # Récupération de aMax(tf)
         xAMax = derv_acc.r
         xAMax = xAMax[xAMax > 0]
         xAMax = xAMax[xAMax < tf]
@@ -81,32 +93,37 @@ def getTfQ(posA, vitA, accA, posB, vitB, accB, vMax, aMax):
         
         aMaxCalc = max(np.abs(acc(xAMax)))
         
+        # Vérification des contrainte de tf (aMax(tf) < aMax et vMax(tf) < vMax) 
         if (vMaxCalc <= vMax) and (aMaxCalc <= aMax):
             return tf
         
         else:
-            i += 1
             tf += pas
     
-    return None
+    return None # valeur de tf > 10 000 s
 
 
 
 
 def polyCommande(posA, vitA, accA, posB, vitB, accB, vMax = [10.0, 10.0, 10.0], aMax = [10.0, 10.0, 10.0]):
+    ''' Fonction retournant les equations de commande des qi'''
     
+    # Le nombre de Qi pour lesquels on doit trouver les equations de commande
     nbQi = len(posA)
     
-    tfs = []
+    tfs = [] # Liste contenant le tf optimal pour chaque qi
     
+    # Récupération des tfs optimaux
     for i in range(nbQi):
         tf = getTfQ(posA[i], vitA[i], accA[i], posB[i], vitB[i], accB[i], vMax[i], aMax[i])
         if tf is None:
             raise ValueError("tf > 10000")
         tfs.append(tf)
     
+    # Conservation du tf maximal pour la sinchronisation des axes
     tf = max(tfs)
     
+    # Récupération des coéficiants des équations de commande
     a0 = geta0(np.array(posA))
     a1 = geta1(np.array(vitA))
     a2 = geta2(np.array(accA))
@@ -119,13 +136,14 @@ def polyCommande(posA, vitA, accA, posB, vitB, accB, vMax = [10.0, 10.0, 10.0], 
     a4 = geta4(A, B, C, tf)
     a5 = geta5(A, B, C, tf)
     
-    pos = []
-    acc = []
-    vit = []
+    pos = []    # liste contenant tout les équation de position pour chaque qi
+    vit = []    # liste contenant tout les équation de vitesse pour chaque qi
+    acc = []    # liste contenant tout les équation de accélération pour chaque qi
+    # Génération des équations de commande pour chaque qi
     for i in range(nbQi):
         pos.append( np.poly1d([a5[i], a4[i], a3[i], a2[i], a1[i], a0[i]]) )
-        acc.append( np.poly1d([5*a5[i], 4*a4[i], 3*a3[i], 2*a2[i], a1[i]]) )
-        vit.append( np.poly1d([20*a5[i], 12*a4[i], 6*a3[i], 2*a2[i]]) )
+        vit.append( np.poly1d([5*a5[i], 4*a4[i], 3*a3[i], 2*a2[i], a1[i]]) )
+        acc.append( np.poly1d([20*a5[i], 12*a4[i], 6*a3[i], 2*a2[i]]) )
     
     return (pos, vit, acc, tf)
 
@@ -134,7 +152,7 @@ def polyCommande(posA, vitA, accA, posB, vitB, accB, vMax = [10.0, 10.0, 10.0], 
 
 if __name__ == "__main__":
                                     #   qA       qA.      qA..     qB       qB.      qB..
-   (pos, acc, vit, tf) = polyCommande([0,0,0], [0,0,0], [0,0,0], [1,2,3], [1,1,1], [0,0,0])
+   (pos, vit, acc, tf) = polyCommande([0,0,0], [0,0,0], [0,0,0], [1,2,3], [1,1,1], [0,0,0])
    
    x = np.arange(0, tf, 0.01)
    for i in range(3):
@@ -143,11 +161,20 @@ if __name__ == "__main__":
        yacc = acc[i](x)
        
        plt.figure(i)
+       
        plt.subplot(3,1,1)
+       plt.title("q"+str(i+1))
+       plt.xticks([])
+       plt.ylabel("Position")
        plt.plot(x, ypos)
        
        plt.subplot(3,1,2)
+       plt.xticks([])
+       plt.ylabel("Vitesse")
        plt.plot(x, yvit)
        
        plt.subplot(3,1,3)
+       plt.ylabel("Acélération")
        plt.plot(x, yacc)
+       
+       plt.xlabel("Time (s)")
